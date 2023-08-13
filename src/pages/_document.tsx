5e2941a00e987e27f80ef08be285031285e7b669
Saad <62691874+saadabd-hub@users.noreply.github.com>
2023-08-14 00:46:44 +0700

    Fix linter issues

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,13 +4,13 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      {(process.env.NODE_ENV === "development" || process.env.VERCEL_ENV === "preview") && (
+        {(process.env.NODE_ENV === 'development' || process.env.VERCEL_ENV === 'preview') && (
             // eslint-disable-next-line @next/next/no-sync-scripts
-            <script
-              data-project-id={process.env.METICULOUS_PROJECT_ID}
-              src="https://snippet.meticulous.ai/v1/meticulous.js"
-            />
-          )}
+          <script
+            data-project-id={process.env.METICULOUS_PROJECT_ID}
+            src="https://snippet.meticulous.ai/v1/meticulous.js"
+          />
+        )}
       <body>
         <Main />
         <NextScript />
