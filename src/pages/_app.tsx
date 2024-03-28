06a01c5247a45e16539ff7eced7cf6eb4930582b
Saad <abdullah.sd48@gmail.com>
2024-03-28 23:30:42 +0700

    Add analytics mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-      <Analytics />
+      <Analytics mode={'production'} />
       <SpeedInsights />
     </>
   )
