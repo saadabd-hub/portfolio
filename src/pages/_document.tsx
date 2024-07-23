c05592d7d40f5cb4965e7e8b7b85c49639a48a9b
Saad <abdullah.sd48@gmail.com>
2024-07-23 20:22:32 +0700

    Update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,41 +1,23 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 import randomBytes from 'randombytes';
-import { promisify } from 'util';
 
-const randomBytesAsync = promisify(randomBytes);
-
-export default async function Document() {
-  const generateNonce = async () => {
-    const bytes = await randomBytesAsync(16);
-    return bytes.toString('base64');
-  };
-
-  const getCspConfig = async () => {
-    const nonce = await generateNonce();
-    return `connect-src 'self' vitals.vercel-insights.com; object-src 'none'; base-uri 'none'; script-src 'self' 'unsafe-inline' 'nonce-${nonce}' 'strict-dynamic'`;
-  };
-
-  const loadMeticulousScript = async (cspConfig: string) => {
-    if (process.env.NODE_ENV === 'development' || process.env.VERCEL_ENV === 'preview') {
-      return (
-        <>
-          <script
-            data-project-id={process.env.METICULOUS_PROJECT_ID}
-            data-is-production-environment="false"
-            src="https://snippet.meticulous.ai/v1/meticulous.js"
-          />
-          <meta httpEquiv='Content-Security-Policy' content={cspConfig} />
-        </>
-      );
-    }
-    return null;
-  };
-
+export default function Document() {
+  const nonce = randomBytes(128).toString('base64');
+  const cspConfig = `connect-src 'self' vitals.vercel-insights.com; object-src 'none'; base-uri 'none'; script-src 'self' 'unsafe-inline' 'nonce-${nonce}' 'strict-dynamic'`
 
   return (
     <Html lang="en">
-      <Head>
-        {loadMeticulousScript(await getCspConfig())}
+      <Head nonce={nonce}>
+        {(process.env.NODE_ENV === 'development' || process.env.VERCEL_ENV === 'preview') && (
+          // eslint-disable-next-line @next/next/no-sync-scripts
+          <>
+            <script
+              data-project-id={process.env.METICULOUS_PROJECT_ID}
+              data-is-production-environment="false"
+              src="https://snippet.meticulous.ai/v1/meticulous.js"
+            />
+            <meta httpEquiv='Content-Security-Policy' content={cspConfig} /></>
+        )}
       </Head>
       <body>
         <Main />
